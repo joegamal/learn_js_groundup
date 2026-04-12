e651f0df3183a83f100edb2797361c93c1cbcdaf
joegamal <yusufmagamalth@gmail.com>
2026-04-12 18:15:45 +0200

    data types in js

---
+++ entry/first.js
@@ -0,0 +1,41 @@
+// single line comment
+// the ; in javaScript is optional
+/**
+ * multi 
+ * line 
+ * comment
+ */
+
+/**
+ * data types in javaScript
+ * 
+ * number
+ * boolean
+ * string
+ * undefined
+ * object
+ * null
+ * 
+ */
+
+var num = 5;
+var firstName = "pop";
+var secName = 'pop';
+/**
+ * no difference between "string" and 'string' in javaScript. 
+ */
+
+/**
+ * any uninitialized used variable in javaScript is undefined 
+ */
+var sec_name;
+var kol = null;
+console.log(typeof num)
+console.log(typeof name) //this is undefined
+console.log(typeof sec_name) // this is undefined also
+console.log(typeof kol) // this is typeof object
+console.log(typeof secName) //this is string 
+
+
+
+
