daab92e248c7d27386022775897721bd3cb8ce89
joegamal <yusufmagamalth@gmail.com>
2026-04-17 18:54:05 +0200

    object prototype intry

--- classes/prototypes.js
+++ classes/prototypes.js
@@ -10,4 +10,11 @@ let person = new Person(40, "nono");
 console.log(person)
 
 let emp = new Employee(44, 'lop', 'adm');
-console.log(emp);
+console.log(emp + '\n####################################\n');
+
+const obj = Object.create(Object);
+console.log(obj);
+console.log(typeof obj)
+console.log(obj instanceof Object)
+console.log(obj.__proto__)
+
